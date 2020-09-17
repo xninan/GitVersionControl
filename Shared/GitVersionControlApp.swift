cb400be306e0883a539127d9f61c8fa54c3a3211
ninan <gifmomllp@gmail.com>
2020-09-17 09:34:18 +0800

    Initial Commit

---
+++ Shared/GitVersionControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitVersionControlApp.swift
+//  Shared
+//
+//  Created by ninan on 2020/9/17.
+//
+
+import SwiftUI
+
+@main
+struct GitVersionControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
